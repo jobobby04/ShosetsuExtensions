af6b4bf57631bdb6e847a07b22620b48083ca274
Jobobby04 <jobobby04@users.noreply.github.com>
2022-08-03 20:21:42 -0400

    Should fix the crash

--- src/en/ReadWN.lua
+++ src/en/ReadWN.lua
@@ -1,4 +1,4 @@
--- {"id":1,"ver":"1.0.34","libVer":"1.0.0","author":"Jobobby04"}
+-- {"id":1,"ver":"1.0.35","libVer":"1.0.0","author":"Jobobby04"}
 
 local baseURL = "https://www.readwn.com"
 local settings = {}
@@ -140,8 +140,13 @@ local function search(filters, reporter)
 		if page == 1 then
 			return parseBrowse(document)
 		else
-			local searchId = selectLast(document:select("ul.pagination a")):attr("href"):match(".*searchid=([0-9]*).*")
-			return parseBrowse(GETDocument(expandURL("/e/search/result/index.php?page=" .. (page - 1) .. "&searchid=" .. searchId)))
+			local pages = document:select("ul.pagination a")
+			if pages:size() > 0 then
+				local searchId = selectLast(document:select("ul.pagination a")):attr("href"):match(".*searchid=([0-9]*).*")
+				return parseBrowse(GETDocument(expandURL("/e/search/result/index.php?page=" .. (page - 1) .. "&searchid=" .. searchId)))
+			else
+				return {}
+			end
 		end
 	end
 	return {}
