cf6ff3833a46949b66aa6a6807ba4e0fa0d46f0f
Jobobby04 <jobobby04@users.noreply.github.com>
2022-08-03 22:28:35 -0400

    Update ReadWN

--- src/en/ReadWN.lua
+++ src/en/ReadWN.lua
@@ -1,4 +1,4 @@
--- {"id":1,"ver":"1.0.41","libVer":"1.0.0","author":"Jobobby04","dep":["ReadWN>=1.0.0"]}
+-- {"id":1,"ver":"1.0.42","libVer":"1.0.0","author":"Jobobby04","dep":["ReadWN>=1.0.0"]}
 
 return Require("ReadWN")("https://www.readwn.com", {
 	id = 1308639964,
