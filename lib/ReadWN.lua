aaed8c66c7ddd83edbc7072eabc0e6b6f5b41e9d
Jobobby04 <jobobby04@users.noreply.github.com>
2022-08-04 14:23:27 -0400

    Re-enable status parsing

--- lib/ReadWN.lua
+++ lib/ReadWN.lua
@@ -1,4 +1,4 @@
--- {"ver":"1.0.8","author":"Jobobby04"}
+-- {"ver":"1.0.9","author":"Jobobby04"}
 
 -- rename this if you ever figure out its real name
 
@@ -96,10 +96,10 @@ function defaults:parseNovel(novelURL, loadChapters)
     local info = NovelInfo {
         title = content:selectFirst(".novel-header .novel-info h1"):text(),
         imageURL = self.expandURL(content:selectFirst(".novel-header .fixed-img img"):attr("data-src")),
-        --[[status = ({
+        status = ({
             Completed = NovelStatus.COMPLETED,
             Ongoing = NovelStatus.PUBLISHING
-        })[selectLast(content:select(".novel-header .novel-info .header-stats span strong")):text()],]]
+        })[self.selectLast(content:select(".novel-header .novel-info .header-stats span strong")):text()],
         description = content:selectFirst("#info .summary"):text(),
         authors = { self.selectLast(content:select(".novel-header .novel-info .author span")):text() },
         genres = categories
