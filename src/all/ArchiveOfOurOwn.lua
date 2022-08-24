d59d069252148cfa17a40480f37419f628c00a2f
Jobobby04 <jobobby04@users.noreply.github.com>
2022-08-23 22:32:24 -0400

    Fix some newlines being removed

--- src/all/ArchiveOfOurOwn.lua
+++ src/all/ArchiveOfOurOwn.lua
@@ -1,4 +1,4 @@
--- {"id":1308639966,"ver":"1.0.1","libVer":"1.0.0","author":"Jobobby04"}
+-- {"id":1308639966,"ver":"1.0.2","libVer":"1.0.0","author":"Jobobby04"}
 
 local baseURL = "https://archiveofourown.org"
 local settings = {}
@@ -40,7 +40,6 @@ local function getPassage(chapterURL)
 	local endNotes = document:selectFirst("#workskin .chapter .end")
 	-- This is for the sake of consistant styling
 	chap:select(".landmark"):remove()
-	chap:select("br:nth-child(even)"):remove()
 	chap = tostring(chap):gsub('<div', '<p'):gsub('</div', '</p'):gsub('<br>', '</p><p>')
 	chap = Document(chap):selectFirst('body')
 	-- Adds Chapter Title
