c543c200da1ca37e3890c078267d74bb6b5b067e
Jobobby04 <jobobby04@users.noreply.github.com>
2024-04-13 16:49:05 -0400

    Add ReadWN clones I could find

--- README.md
+++ README.md
@@ -2,16 +2,22 @@
 
 Personal extensions for Shosetsu
 
-#### Add the repo to shosetsu using this Url:
+#### Add the repo to Shosetsu using this Url:
 
 https://raw.githubusercontent.com/jobobby04/ShosetsuExtensions/master
 
 
 ### Repo Extensions
 - [ArchiveOfOurOwn](https://archiveofourown.org/)
+- [Hentai Foundry](https://www.hentai-foundry.com/)
+- [Literotica](https://www.literotica.com/)
 - [ReadWN](https://www.readwn.com/)
 - [LightNovelMeta](https://www.lightnovelmeta.com/)
 - [Wuxiap](https://www.wuxiap.com/)
 - [FanNovel](https://www.fannovel.com/)
-- [Hentai Foundry](https://www.hentai-foundry.com/)
-- [Literotica](https://www.literotica.com/)
+- [FansMTL](https://www.fansmtl.com/)
+- [WuxiaFox](https://www.wuxiafox.com/)
+- [LtNovel](https://www.ltnovels.com/)
+- [WuxiaOne](https://www.wuxiaone.com/)
+- [WuxiaBox](https://www.wuxiabox.com/)
+- [WuxiaMate](https://www.wuxiamate.com/)
---
+++ src/en/WuxiaFox.lua
@@ -0,0 +1,108 @@
+-- {"id":1308639972,"ver":"1.0.0","libVer":"1.0.0","author":"Jobobby04","dep":["ReadWN>=1.0.11"]}
+
+local GENRES = {
+    "All",
+    "Action",
+    "Adventure",
+    "Comedy",
+    "Contemporary Romance",
+    "Drama",
+    "Eastern Fantasy",
+    "Ecchi",
+    "Fantasy",
+    "Fantasy Romance",
+    "Gender Bender",
+    "Harem",
+    "Historical",
+    "Horror",
+    "Josei",
+    "Lolicon",
+    "Magical Realism",
+    "Martial Arts",
+    "Mecha",
+    "Mystery",
+    "Psychological",
+    "Romance",
+    "School Life",
+    "Sci-fi",
+    "Seinen",
+    "Shoujo",
+    "Shounen",
+    "Shounen Ai",
+    "Slice of Life",
+    "Smut",
+    "Sports",
+    "Supernatural",
+    "Tragedy",
+    "Video Games",
+    "Wuxia",
+    "Xianxia",
+    "Xuanhuan",
+    "Yaoi",
+    "Modern Romance",
+    "Historical Romance",
+    "Gay Romance",
+    "Game",
+    "Games",
+    "Romantic",
+    "Military",
+    "Urban",
+    "Farming",
+    "Modern Life",
+    "Urban Life",
+    "Billionaire",
+    "CEO",
+    "Chinese",
+    "Korean",
+    "Japanese",
+    "Yuri",
+    "Two-dimensional",
+    "Shoujo Ai",
+    "Fan-Fiction",
+    "Erciyuan",
+    "Isekai",
+    "Magic",
+    "Virtual Reality",
+    "Faloo",
+}
+
+return Require("ReadWN")("https://www.wuxiafox.com", {
+    id = 1308639972,
+    name = "WuxiaFox",
+    shrinkURLNovel = "^.-wuxiafox%.com",
+    hasCloudFlare = true,
+
+    genres = GENRES,
+
+    listingsMap = {
+        {
+            name = "Recently Added Chapters",
+            increments = false,
+            selector = "#latest-updates .novel-list.grid.col .novel-item a",
+            url = function(data)
+                return "https://www.wuxiafox.com"
+            end
+        },
+        {
+            name = "Popular Daily Updates",
+            increments = true,
+            url = function(data)
+                return "https://www.wuxiafox.com/list/all/all-lastdotime-" .. (data[PAGE] - 1) .. ".html"
+            end
+        },
+        {
+            name = "Most Popular",
+            increments = true,
+            url = function(data)
+                return "https://www.wuxiafox.com/list/all/all-onclick-" .. (data[PAGE] - 1) .. ".html"
+            end
+        },
+        {
+            name = "New to Web Novels",
+            increments = true,
+            url = function(data)
+                return "https://www.wuxiafox.com/list/all/all-newstime-" .. (data[PAGE] - 1) .. ".html"
+            end
+        }
+    },
+})
