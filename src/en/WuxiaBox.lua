8ca2989b4eedd5b7bbf2f6ee198b4aa4081a796e
Jobobby04 <jobobby04@users.noreply.github.com>
2024-04-14 12:56:22 -0400

    LightNovelMeta -> WuxiaMate
    # Conflicts:
    #	index.json
    #	src/en/WuxiaBox.lua

--- src/en/WuxiaBox.lua
+++ src/en/WuxiaBox.lua
@@ -1,4 +1,4 @@
--- {"id":1308639975,"ver":"1.1.6","libVer":"1.3.0","author":"Jobobby04","dep":["ReadWN>=1.0.11"]}
+-- {"id":1308639964,"ver":"1.1.6","libVer":"1.3.0","author":"Jobobby04","dep":["ReadWN>=1.0.11"]}
 
 local GENRES = {
     "All",
