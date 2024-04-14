9452fd489aeb823fa7a48ff56725a068d141c590
Jobobby04 <jobobby04@users.noreply.github.com>
2024-04-14 12:39:47 -0400

    Migrate from ReadWN to WuxiaBox

--- src/en/WuxiaBox.lua
+++ src/en/WuxiaBox.lua
@@ -1,4 +1,4 @@
--- {"id":1308639975,"ver":"1.0.0","libVer":"1.0.0","author":"Jobobby04","dep":["ReadWN>=1.0.11"]}
+-- {"id":1308639975,"ver":"1.1.5","libVer":"1.0.0","author":"Jobobby04","dep":["ReadWN>=1.0.11"]}
 
 local GENRES = {
     "All",
@@ -62,7 +62,7 @@ local GENRES = {
 }
 
 return Require("ReadWN")("https://www.wuxiabox.com", {
-    id = 1308639975,
+    id = 1308639964,
     name = "WuxiaBox",
     shrinkURLNovel = "^.-wuxiabox%.com",
     hasCloudFlare = true,
