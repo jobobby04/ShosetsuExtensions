0cf948e177ae638a25c9bacd07e926c3ce8385bf
Jobobby04 <jobobby04@users.noreply.github.com>
2024-05-20 13:25:49 -0400

    Better title selection

--- src/all/ArchiveOfOurOwn.lua
+++ src/all/ArchiveOfOurOwn.lua
@@ -1,4 +1,4 @@
--- {"id":1308639966,"ver":"1.0.6","libVer":"1.3.0","author":"Jobobby04"}
+-- {"id":1308639966,"ver":"1.0.7","libVer":"1.3.0","author":"Jobobby04"}
 
 local baseURL = "https://archiveofourown.org"
 local settings = {}
@@ -289,7 +289,7 @@ local function search(filters)
 		local novel = ClientGetDocument(novelUrl)
 		return {
 			NovelInfo {
-				title = novel:selectFirst(".title"):text(),
+				title = novel:selectFirst(".preface > .title"):text(),
 				link = shrinkURL(novelUrl),
 				imageURL = ""
 			}
